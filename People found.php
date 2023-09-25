5f7793904f1dc4d2db19bfc15ce2b2c685870d8c
saifsami78 <143657199+saifsami78@users.noreply.github.com>
2023-09-25 13:36:35 +0600

    Just a better look

--- People found.php
+++ People found.php
@@ -1,127 +1,82 @@
-<?php
-
-session_start();
-
-if(!isset($_SESSION['username'])){
-    echo "<script>top.window.location = './index.php'</script>";
-}
-
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Campus Connect </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  </head>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Campus Connect</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <style>
+    .product-card {
+      margin-bottom: 20px;
+    }
 
-  
-  <body>
-     <?php include 'navbar.php'; ?>
-        <br>
-        <br>
-      <div class="cotainer" >
-        <style>
-          h3{
-            color: mediumseagreen;
-          }
-          h6{
-            color: mediumseagreen !important;
-          }
-        </style>
-        
-          <h3> <center> All The Stuff People Has got </center></h3>
-          <br>
-          <h6><center><u>All the items that people have found are listed below. If what you've lost is included in the list, please note down the product ID and click on 'More Details' to access additional information and find out how to contact them </u></center></h6>
-      
-      </div>
+    .product-card h2 {
+      font-size: 18px;
+      margin-top: 0;
+    }
 
-        <br>
-      
-    
-        <div class="container">
-          <style>
-            .product {
-                border: 1px solid #ccc;
-                padding: 10px;
-                margin-bottom: 20px;
-                background-color: #f9f9f9;
-            }
+    .product-card p {
+      font-size: 14px;
+      margin-bottom: 0;
+    }
 
-            .product h2 {
-                font-size: 18px;
-                margin-top: 0;
-            }
+    .no-results {
+      font-size: 16px;
+      font-weight: bold;
+      color: red;
+    }
+  </style>
+</head>
 
-            .product p {
-                font-size: 14px;
-                margin-bottom: 0;
-            }
+<body>
+  <?php include 'navbar.php'; ?>
 
-            .no-results {
-                font-size: 16px;
-                font-weight: bold;
-                color: red;
-            }
-          </style>
+  <br>
+  <br>
 
-        <?php
-        
-        
+  <div class="container">
+    <h3 class="text-center">All The Stuff People Has got</h3>
+    <h6 class="text-center text-muted">All the items that people have found are listed below. If what you've lost is included in the list, please click on the product name to learn more.</h6>
+
+    <div class="row">
+      <?php
         $conn = mysqli_connect('localhost', 'root', '', 'connect');
 
-
         if ($conn === false) {
-            die('Connection failed: ' . mysqli_connect_error());
+          die('Connection failed: ' . mysqli_connect_error());
         }
-
 
         $sql = 'SELECT product_name, post_id FROM lost_and_found';
         $result = mysqli_query($conn, $sql);
 
-
         if (mysqli_num_rows($result) > 0) {
           while ($row = mysqli_fetch_assoc($result)) {
             $post_id = $row['post_id'];
-        
+
             // Create a link to the page `activatemoredetails.php` with the `post_id` query parameter
             $link = "activatemoredetails.php?post_id=$post_id";
-        
-            // Wrap the product name in a link
-            echo "<a href='$link'>" . $row['product_name'] . "</a>";
-        
-            // Print the rest of the product information
-            echo '<p>Post ID: ' . $row['post_id'] . '</p>';
-            echo '</div>';
+
+            echo '<div class="col-md-4">
+              <div class="product-card card">
+                <div class="card-body">
+                  <h2 class="card-title">' . $row['product_name'] . '</h2>
+                  <a href="' . $link . '" class="btn btn-primary">More details</a>
+                </div>
+              </div>
+            </div>';
           }
         } else {
           echo '<p class="no-results">No products found.</p>';
         }
-      
-
 
         mysqli_close($conn);
+      ?>
+    </div>
+  </div>
 
-        ?>
-        </div>
-        
+  <br>
 
-        <div class="container">
-            <a href="More details.php" class="btn btn-success"> More Details</a>
-      
-        </div>
+  <?php include 'footer.html'; ?>
+</body>
 
-        
-
-
-
-   
-  
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    <br>
-    <?php include 'footer.html'; ?>
-  </body>
 </html>
